c7aef55c0b6181a261f10d80ecd0d26d3c959d90
benjamin337rpm <benjamin337rpm@gmail.com>
2023-01-25 11:16:00 -0300

    first commit

---
+++ js/api.js
@@ -0,0 +1,17 @@
+// const token = '211aa8b1a58a3361cfed512347bdbeec'
+// let url = `https://api.themoviedb.org/3/movie/157336?api_key=${token}`
+
+
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
+
+// fetch(url, requestOptions)
+//   .then(response => response.text())
+//   .then(result =>{
+//     console.log(JSON.parse(result))
+//     let dados = JSON.parse(result)
+//     document.getElementById('trocaNome').innerHTML=dados.title
+//   })
+//   .catch(error => console.log('error', error));
